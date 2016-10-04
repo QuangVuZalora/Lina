797b5c45adf1429e8f7899cfdecd5595a451ff53
tuan <tuan.nguyen@zalora.com>
2016-10-04 17:50:15 +0700

    Save image to disk

--- Slayer/models.py
+++ Slayer/models.py
@@ -3,3 +3,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ImageFile(models.Model):
+    image = models.ImageField(upload_to = 'img/', default = 'img/no-img.jpg')
